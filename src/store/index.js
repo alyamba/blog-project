a378f72ca032baec2349a0e6b705c74b2ae4d3eb
Alina <logunionok@gmail.com>
2023-01-19 21:10:40 +0300

    fix: delete comments and unnecessary parts of code

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
-import { combineReducers, configureStore, createStore } from "@reduxjs/toolkit";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { combineReducers, createStore } from "@reduxjs/toolkit";
 import { postReducer } from "./postReducer";
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
@@ -15,15 +14,5 @@ const rootReducer = combineReducers({
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// export const store = configureStore({
-//   reducer: persistReducer,
-//   middleware: (getDefaultMiddleware) => {
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     });
-//   },
-// });
 export const store = createStore(persistedReducer);
 export const persistor = persistStore(store);
